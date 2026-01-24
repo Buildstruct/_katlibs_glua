ee56f626c62f15ae8e31f0ebef7dbddf77cf3a84
charred.ember <cember.gmod@gmail.com>
2026-01-24 13:15:03 -0600

    KTimeUtils

---
+++ lua/_katlibs_glua/universal/ktimeutils.lua
@@ -0,0 +1,38 @@
+---UNIVERSAL, STATIC<br>
+---Time related utilities for program control flow and animation.
+KTimeUtils = {}
+
+local co_yield = coroutine.yield
+local co_running = coroutine.running
+local CurTime = CurTime
+local assert = assert
+
+local KError_ValidateArg = KError.ValidateArg
+local KVarCondition_NumberGreater = KVarCondition.NumberGreater
+local KVarCondition_Function = KVarCondition.Function
+
+---A tween that runs inside a coroutine.<br>
+---Blocks the current thread until the duration has finished.
+---@param duration number
+---@param func fun(up: number)
+function KTimeUtils.TweenAsync(duration,func)
+    assert(co_running(),"ThreadedTween called outside of coroutine!")
+    KError_ValidateArg(1,"duration",KVarCondition_NumberGreater(duration,0))
+    KError_ValidateArg(2,"func",KVarCondition_Function(func))
+
+    local savedTime = CurTime()
+    local DT
+
+    while true do
+        DT = CurTime() - savedTime
+
+        if DT <= duration then
+            func(DT / duration)
+        else
+            func(1)
+            break
+        end
+
+        co_yield()
+    end
+end
